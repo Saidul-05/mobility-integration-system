fde20bbaa84e42b90b65132f207500cc1365173c
Saidul-05 <139872397+Saidul-05@users.noreply.github.com>
2025-02-23 02:12:23 +0600

    Create views.py

---
+++ django_app/main/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from .models import Service
+from .serializers import ServiceSerializer
+
+class ServiceListCreateView(generics.ListCreateAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
+class ServiceDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
