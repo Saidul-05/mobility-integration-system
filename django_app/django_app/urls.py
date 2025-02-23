c3de7466d98f6edcac87df52296e6bac50baed18
Saidul-05 <139872397+Saidul-05@users.noreply.github.com>
2025-02-23 13:52:23 +0600

    Update urls.py

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("main.urls")),
+    path('admin/', admin.site.urls),
+    path('api/', include('main.urls')),    # e.g. /api/services/
+    path('api/users/', include('users.urls')),  # e.g. /api/users/login
 ]
