400e9defe6c6d16a9511aa3aa67efcd5c964470f
Saidul-05 <139872397+Saidul-05@users.noreply.github.com>
2025-02-23 15:30:28 +0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+
+// Pages
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Checkout from './pages/Checkout';
 
 function App() {
   return (
     <div>
+      {/* Common navigation bar */}
       <Navbar />
+
+      {/* Define all your routes here */}
       <Routes>
+        {/* Home page */}
         <Route path="/" element={<Home />} />
+
+        {/* Auth pages */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
+        {/* Checkout page (serviceId is hardcoded as 1 for example) */}
+        <Route path="/checkout" element={<Checkout serviceId={1} />} />
       </Routes>
     </div>
   );
