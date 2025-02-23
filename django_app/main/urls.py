8f473ceb60638dc8e0bf9e0de55247242d33fc1c
Saidul-05 <139872397+Saidul-05@users.noreply.github.com>
2025-02-23 13:55:34 +0600

    Update urls.py

--- django_app/main/urls.py
+++ django_app/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ServiceListCreateView, ServiceDetailView
+from .views import ServiceListCreateView, ServiceDetailView, StripeCheckoutView
 
 urlpatterns = [
-    path("services/", ServiceListCreateView.as_view(), name="service-list"),
-    path("services/<int:pk>/", ServiceDetailView.as_view(), name="service-detail"),
+    path('services/', ServiceListCreateView.as_view(), name='service-list'),
+    path('services/<int:pk>/', ServiceDetailView.as_view(), name='service-detail'),
+    path('checkout/', StripeCheckoutView.as_view(), name='stripe-checkout'),
 ]
