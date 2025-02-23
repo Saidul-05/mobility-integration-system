a750735baf59d97e89647f199c177576c944bb95
Saidul-05 <139872397+Saidul-05@users.noreply.github.com>
2025-02-23 14:00:11 +0600

    Create views.py

---
+++ django_app/users/views.py
@@ -0,0 +1,19 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        user = self.serializer_class.Meta.model.objects.get(id=response.data['id'])
+        refresh = RefreshToken.for_user(user)
+        response.data['refresh'] = str(refresh)
+        response.data['access'] = str(refresh.access_token)
+        return response
