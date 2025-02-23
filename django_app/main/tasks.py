9f9c71de2156d93a329788419fd8b9f022fa99c1
Saidul-05 <139872397+Saidul-05@users.noreply.github.com>
2025-02-23 14:46:36 +0600

    Update tasks.py

--- django_app/main/tasks.py
+++ django_app/main/tasks.py
@@ -2,5 +2,4 @@ from celery import shared_task
 
 @shared_task
 def example_task():
-    print("Celery task running every 5 minutes")
-    # Do something, e.g. send emails, cleanup, etc.
+    print("Celery periodic task is running.")
